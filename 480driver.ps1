7c643ede3f56c11bbb1bd51a547a058cada71667
DeanPalovich <123338565+DeanPalovich@users.noreply.github.com>
2024-03-06 02:47:37 -0500

    IT WORKS

--- 480driver.ps1
+++ 480driver.ps1
@@ -1,9 +1,23 @@
 Import-Module '480-utils' -Force
+
+#Call the banner function
 480Banner
-$conf = Get-480Config - config_path = "/home/dean/Documents/GitHub/tech-journal1/480.json"
+
+$conf = Get-480Config -config_path '/home/dean/Documents/GitHub/tech-journal1/480.json'
 480Connect -server $conf.vcenter_server
-$vm =Select-VM -folder $conf.vm_folder
-$db = Select-DB
-$snapshot = Get-Snapshot -VM $vm.Name | Select-Object -First 1
-$network = Select-Network -vm $vm.Name
-FullClone -vm $vm.Name -snap $snapshot -vmhost $config.esxi_host -ds $db -network $network
+
+
+#Select VM Function
+Write-Host "Selecting your VM"
+Select-VM -folder "BASEVM"
+
+
+
+# Call the cloner
+Write-Host "Clone a VM"
+$shallBeCloned = Read-Host -Prompt "Enter the name of the vm you wish to clone"
+$baseVM = Read-Host -Prompt "Enter the name of the base vm of the vm you wish to clone"
+$newVMName = Read-Host -Prompt "Enter the name of your new vm"
+Cloner -shallBeCloned $shallBeCloned -baseVM $baseVM -newVMName $newVMName
+Get-VM
+
--- modules/480-utils/480-utils.psm1
+++ modules/480-utils/480-utils.psm1
@@ -1,172 +1,93 @@
-function 480Banner()
-{
-    Write-host "Hello SYS480 DevOps"
+function 480Banner(){
+    Write-host "⠀⠀⠀⠀
+         ⠀⢀⣏⣉⣿⣿⣿⣿⣿⣦⡠⠔⠒⣒⡶⠶⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⢸⡟⣿⣿⣿⣿⣿⣿⣿⣷⣤⠎⠁⠀⠀⢸⠈⢢⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠈⣟⠻⣛⠿⠯⢍⡙⠛⠛⠿⣦⡀⠠⠀⣸⠀⠀⡇⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢨⠋⠀⠀⠀⠀⠈⠳⣀⠞⠁⠀⠀⠀⠀⠑⢄⡹⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⢀⣀⠀⡏⠀⣀⠀⠀⠀⠀⠀⠈⡅⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢇⠀⠀⠀⠀⢿⣿⢃⢇⢸⣿⡷⠀⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⢀⠎⠒⠄⣀⡀⣀⠠⠊⠈⠢⣀⠀⠀⠀⠀⡠⠚⡄⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⡠⠒⡌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠁⠀⠀⢱⢀⡀⠀⠀⠀⠀
+    ⠀⠀⠀⠁⠀⠇⠀⠀⠀⠀⢸⣑⠶⠶⠲⠶⢶⣲⠆⠀⠀⠀⠀⢸⠀⠸⠐⠂⠀⠀
+    ⠀⠀⠀⠀⠀⠘⡀⠀⠀⠀⠘⡅⠀⠀⠀⠀⠀⡝⠀⠀⠀⠀⠀⡜⠀⠀⠀⠀⠀⠀
+    ⠈⢆⠀⠀⠀⠀⠘⠤⡀⠀⠀⠘⠤⣀⣀⡠⠞⠀⠀⠀⠀⡠⠊⠀⠀⠀⠀⠀⠀⠂
+    ⠀⠀⠣⡀⠀⠀⠀⠀⣼⣷⣦⣤⣤⣄⣀⣀⣀⣤⣤⣶⣿⣦⠀⠀⠀⠀⠀⡠⠂⠀
+    ⠀⠀⠀⠈⠂⢄⡀⡸⣿⡿⠟⢿⣿⣿⣿⡿⠟⢿⣿⣿⣿⡿⢃⠀⢀⠠⠊⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢨⠁⠉⢀⣤⡀⠙⠟⠉⢀⣄⠀⠉⠛⠉⣀⣼⡒⠁⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢸⣷⣾⣿⣿⣿⣦⣤⣶⣿⣿⣷⣦⣤⣾⣿⣿⡇------Hello SYS480-Devops From Dean"
 }
 
-function 480Connect([string] $server)
-{
+function 480Connect([string] $server){
     $conn = $global:DefaultVIServer
+    #are we already connected
     if ($conn){
-        $msg = "Already Connected to: {0}" -f $conn
+        $msg = 'Already Connected to: {0}' -f $conn
 
         Write-Host -ForegroundColor Green $msg
-    }else
-    {
-        $conn = Connect-VIServer -Server $server
     }
-
+    else{
+        $conn = Connect-VIServer -Server $server
+        #if this fails, let Connect-VIServer handle the encryption
+    }
 }
 
-Function Get-480Config([string] $config_path)
-{
-    
-    $conf=$null
-    if(Test-Path $config_path)
-    {
+function Get-480Config([string] $config_path){
+    Write-Host "Reading " $config_path
+    $conf = $null
+    if (Test-Path $config_path){
         $conf = (Get-Content -Raw -Path $config_path | ConvertFrom-Json)
-        $msg = "Using Configuration at {0}" -f $config_path
-        Write-Host -ForegroundColor "Green" $msg 
-    } else 
-    {
-        Write-Host -ForegroundColor "Yellow" "No Configuration"
+        $msg = "Using configuration at {0}" -f $config_path
+        Write-Host -ForegroundColor Green $msg
+    }
+    else{
+        Write-Host -ForegroundColor "Yellow" "No Configurtion"
     }
     return $conf
 }
-Function ErrorHandling($index, $maxIndex)
-{
-    if ($index -ge 1 -and $index -le $maxIndex)
-    {
-        return $true
-    }
-    else {
-        Write-Host "Invlaid index. Please enter an index between 1 and $maxIndex" -ForegroundColor "yellow"
-        return $false
-    }
-}
-Function Select-VM([string] $folder)
-{
-    Write-Host "Select VM"
-    $Selected_vm=$null
-    try 
-    {
+
+function Select-VM([string] $folder){
+    $selected_vm=$null
+    try{
         $vms = Get-VM -Location $folder
         $index = 1
-        if ($vms.Count -eq 0){
-            Write-Host "No VMS found " -ForegroundColor "Red"
-            return $null
-        }
-        foreach($vm in $vms)
-        {
-            Write-Host [$index] $vm.Name
+        foreach($vm in $vms){
+            Write-Host [$index] $vm.name
             $index+=1
-        }    
-        
-        do
-        {
-            $pick_index =Read-Host "which index number [x] do you wish to pick?"
-            if ($pick_index -eq "") {
-                Write-Host "Please enter a valid index." -ForegroundColor "yellow"
-                Continue
-            }
-            if (ErrorHandling -index $pick_index -maxIndex $vms.Count)
-            {
-                $Selected_vm = $vms[$pick_index - 1]
-                Write-Host "you picked $($Selected_vm.Name)"
-            }
-        }while (-not $Selected_vm)
-        return $Selected_vm
         }
-        catch
-        {
+        $pick_index = Read-Host "Which index number [x] do you wish to pick?"
+        if($pick_index -ge $index){
+            return "Error, you selected a VM that does not exist" 
+        }        
+        $selected_vm = $vms[$pick_index -1]
+        Write-Host "You picked " $selected_vm.name
+        #note this is a full on vm object that we can interact with
+        return $selected_vm
+    }
+
+    catch{
         Write-Host "Invalid Folder: $folder" -ForegroundColor "Red"
-        }
+    }
 }
-function Select-DB()
-{
-    Write-Host "Select your Datastore "
-   $selected_db = $null
 
-   $datastores = Get-Datastore
-   $index = 1
-
-   if ($datastores.Count -eq 0) 
-   {
-    Write-Host "No Datastore are found. " -ForegroundColor "Red"
+function Cloner([string] $shallBeCloned, [string] $baseVM, [string] $newVMName){
+    try{
+      Write-Host $shallBeCloned
+      Write-Host $baseVM
+      Write-Host $newVMName
     
-   }
-
-   foreach ($ds in $datastores)
-    {
-        Write-Host [$index] $ds.Name
-        $index += 1
+      $vm = Get-VM -Name $shallBeCloned
+      $snapshot = Get-Snapshot -VM $vm -Name $baseVM
+      $vmhost = Get-VMHost -Name "192.168.7.26"
+      $ds = Get-DataStore -Name "datastore1-super16"
+      $linkedClone = "{0}.linked" -f $vm.name
+      $linkedVM = New-VM -LinkedClone -Name $linkedClone -VM $vm -ReferenceSnapshot $snapshot -VMHost $vmhost -Datastore $ds
+      $newvm = New-VM -Name "$newVMName.base" -VM $linkedVM -VMHost $vmhost -Datastore $ds
+      $newvm | New-Snapshot -Name "Base"
+      $linkedvm | Remove-VM
     }
-   
-    do 
-    {
-        $choice = Read-Host "which index number [x] do you wish to pick?"
-        if (ErrorHandling -index $choice -maxIndex $datastores.Count) {
-            $selected_db = $datastores[$choice - 1]
-            Write-Host "You picked " $selected_db.Name
-        }
-    } while ($null -eq $selected_db) 
-
-    return $selected_db
-    
-}
-function Select-Network([string] $vm)
-{
-    Write-Host "Select your Network "
-    $selected_net=$null
-
-    $networks = Get-NetworkAdapter -VM $vm
-    $index=1
-
-    if ($networks.Count -eq 0) 
-    {
-        Write-Host "No VMs were found " -ForegroundColor "Red"
-        return $null
+    catch {
+      Write-Host "ERROR"
+      exit
     }
+  }
 
-    foreach($net in $networks)
-    {
-        Write-Host [$index] $net.Name
-        $index+=1
-    }
-
-    do
-    {
-        $choice = Read-Host "Which index number [x] do you wish to pick?"
-        if (ErrorHandling -index $choice -maxIndex $networks.Count)
-        {
-            $selected_net = $networks[$choice - 1]
-            Write-Host "You picked " $selected_net.Name
-        }
-    } While ($null -eq $selected_net)
-    {
-        return $selected_net
-    }
-
-}
-
-function FullClone([string] $vm, $snap, $vmhost, $ds, $network)
-{
-    $linkedName = "{0}.linked" -f $vm
-    $linkedVM = New-VM -LinkedClone -Name $linkedName -VM $vm -ReferenceSnapshot $snap -VMHost $vmhost -Datastore $ds
-    $newvmname = Read-Host -prompt "Enter name for new VM"
-    $newVM = -Name $newvmname -VM $linkedVM -VMHost $VMHost $vmhost -Datastore $ds
-    $newVM | Set-NetworkAdapter -NetworkName $network
-    $newVM | New-Snapshot -Name "Base"
-    $linkedvm | Remove-VM
-    $powerOp = Read-Host "would you like turn on the machine" $newVM.Name "(Y/N)?"
-    if ($powerOp -match "^[yY]$")
-    {
-        Start-VM -VM $newVM
-        Write-Host $newVM.Name "has turned on"
-    } else
-    {
-        break
-    }
-}
-
-
-    
